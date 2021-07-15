c123270ebbded222e48602fe73dcc06368fa9ffa
ajfame11 <ajfame11@gmail.com>
2021-07-15 15:12:57 -0700

    set up auth controller

---
+++ backend/app/controllers/auth_controller.rb
@@ -0,0 +1,17 @@
+class AuthController < ApplicationController
+    before_action :authorized, only: [:profile]
+  
+    def create
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        token = encode_token({user_id: user.id})
+        render json: { jwt: token, user: user }
+      else
+        render json: {error: "Incorrect username or password!"}, status: :unauthorized
+      end
+    end
+  
+    def profile
+      render json: @user
+    end
+end
