1ba1537aac48d75ff55fd48a1a52c8c91fef27ac
ajfame11 <ajfame11@gmail.com>
2021-07-15 16:46:08 -0700

    set up post controller

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,16 +1,25 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
+  before_action :find_user
+  before_action :authorized
 
   # GET /posts
   def index
     @posts = Post.all
 
     render json: @posts
+
+    # @posts = @user.posts
+    # render json: @posts
   end
 
   # GET /posts/1
   def show
     render json: @post
+
+    
+    # @post = @user.posts.find_by(id: params[:id])
+    # render json: @post
   end
 
   # POST /posts
@@ -20,22 +29,28 @@ class PostsController < ApplicationController
     if @post.save
       render json: @post, status: :created, location: @post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: {errors: "Post could not be created"}
     end
   end
 
   # PATCH/PUT /posts/1
   def update
-    if @post.update(post_params)
+    if @post = current_user.posts.find_by_id(params[:id])
+      @post.update(post_params)
       render json: @post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: {errors: "Post could not be updated"}
     end
   end
 
   # DELETE /posts/1
   def destroy
-    @post.destroy
+    if @post = current_user.posts.find_by_id(params[:id])
+      @post.destroy
+    else
+      render json: {errors: "Post could not be deleted"}
+    end
+
   end
 
   private
@@ -48,4 +63,9 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:title, :body, :image, :user_id)
     end
+
+    def find_user
+      @user = User.find_by(id: params[:user_id])
+    end
+
 end
