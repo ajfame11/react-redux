6933d2f90abf72c3234ad2fae7e5bd2c814f5737
ajfame11 <ajfame11@gmail.com>
2021-07-15 17:01:27 -0700

    fixed routes for login

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
   resources :users
+
+  post '/authenticate', to: 'auth#create'
+  get '/profile', to: 'auth#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
