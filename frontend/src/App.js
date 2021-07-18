baffffb3f8e6f614bf1bf1575c556ac141365e47
ajfame11 <ajfame11@gmail.com>
2021-07-18 08:05:44 -0700

    single html file

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+//style
+import './App.css';
+
+// packages
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom'
+import { connect } from 'react-redux'
+
+// components
+import Home from "./components/Home"
+
+
+class App extends React.Component {
+  
+    render() {
+      return (
+        <Router>
+        <div className="App">
+  
+        <Switch>
+  
+        <Route exact path="/" render={() => <Home />} />
+  
+        </Switch>
+  
+        </div>
+        </Router>
+      );
+    }
+  }
+  
